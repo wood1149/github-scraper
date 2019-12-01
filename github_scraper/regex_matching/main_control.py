1833220aab9afb6a76980a914a53f6c1be99b23c
nathan <sardo.9@osu.edu>
2019-12-01 15:43:31 -0500

    updates files and naming

---
+++ github_scraper/regex_matching/main_control.py
@@ -0,0 +1,45 @@
+from regex_matching_strings import match_api_key as MatchAPI
+from regex_matching_strings import match_password as MatchPW
+from regex_matching_strings import match_crypto as MatchCrypto
+from regex_matching_strings import match_bitcoin as MatchBitcion 
+from regex_matching_strings import match_email as MatchEmail
+
+gfilestr = ""
+#takes in a map, runs regex matching on text, and returns type/line number of match
+def main(fileMap):
+    outputMap = {}
+    for key in fileMap:
+        outputMap[key] = []
+        findMatches(key,fileMap[key],outputMap)
+        
+    return outputMap
+
+
+def getLine(filestr=gfilestr):
+    prevnl = -1
+    while True:
+      nextnl = filestr.find('\n', prevnl + 1)
+      if nextnl < 0: break
+      yield filestr[prevnl + 1:nextnl]
+      prevnl = nextnl
+
+def findMatches(key,fileString,outputMap):
+    print("finding matches")
+    idx = 1
+    for line in getLine(fileString):
+      if(MatchAPI(line)):
+        outputMap[key] = ["API:" + str(idx)]
+      if(MatchPW(line)):
+        outputMap[key] = ["Password:" + str(idx)]
+      if(MatchCrypto(line)):
+        outputMap[key] = ["Crypto:" + str(idx)]
+      if(MatchBitcion(line)):
+        outputMap[key] = ["Bitcoin:" + str(idx)]
+      if(MatchEmail(line)):
+        outputMap[key] = ["Email:" + str(idx)]
+
+        idx +=1
+            
+
+if __name__ == '__main__':
+    pass
