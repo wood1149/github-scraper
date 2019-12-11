61240a95901eba9d249cf42c41e5f1ac991692b4
Kevin Klose <kevinklose23@gmail.com>
2019-12-10 19:47:11 -0500

    Cleaning up some of get user data script

--- get-user-data.py
+++ get-user-data.py
@@ -4,10 +4,11 @@ from github_scraper.utilities import display_results, save_results
 from collections import defaultdict, Counter
 import os
 
+headers = {}
 
-api_token = '2a39969df84ade22a5871b3db2c1c70c17de4171'
-headers = {'Authorization': f'token {api_token}'}
-#headers = {}
+# Once API token is obtained from Github
+#api_token = '*'
+#headers = {'Authorization': f'token {api_token}'}
 
 username_path = 'username_data'
 username_dir = os.fsencode(username_path)
@@ -15,32 +16,31 @@ username_dir = os.fsencode(username_path)
 all_data = Counter()
 per_user_data = Counter()
 
-count = 0
+user_count = 0
 
 for f in os.listdir(username_dir):
     file_path = username_path + '/' + os.fsdecode(f)
     with open(file_path, 'r') as read_file:
         for line in read_file:
-            count += 1
-            if count >= 1 and count <= 2000:
-                curr_user_data = Counter()
-                username = line.strip()
+            user_count += 1
+            curr_user_data = Counter()
+            username = line.strip()
 
-                print(f'Getting all files for user {username}')
-                repo_files = RepoProcessing.get_all_files_for_user(username, headers)
-                print(f'Recieved all files from {username}')
-                results = find_vulnerabilities(repo_files)
-                for file, v_list in results.items():
-                    if  v_list:
-                        for v in v_list:
-                            v_type, _, line = v.partition(':')
-                            curr_user_data[v_type] += 1
+            print(f'Getting all files for user {username}')
+            repo_files = RepoProcessing.get_all_files_for_user(username, headers)
+            print(f'Recieved all files from {username}')
+            results = find_vulnerabilities(repo_files)
+            for file, v_list in results.items():
+                if  v_list:
+                    for v in v_list:
+                        v_type, _, line = v.partition(':')
+                        curr_user_data[v_type] += 1
 
-                all_data = all_data + curr_user_data
-                for key in curr_user_data:
-                    per_user_data[key] += 1
+            all_data = all_data + curr_user_data
+            for key in curr_user_data:
+                per_user_data[key] += 1
 
 
-                print(f'All Data: {all_data}')
-                print(f'Per User Data: {per_user_data}')
-                print(f'Username count: {count}')
+            print(f'All Data: {all_data}')
+            print(f'Per User Data: {per_user_data}')
+            print(f'Username count: {user_count}')
